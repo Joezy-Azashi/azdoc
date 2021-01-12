996d2fc1745fcef7275a26131a1cbe13c5d3ccac
Vincent Aboagye <68636740+Vincent-Aboagye@users.noreply.github.com>
2021-01-12 14:00:28 +0000

    Signout form refactored

--- azdoc/src/pages/forms/signoutform.js
+++ azdoc/src/pages/forms/signoutform.js
@@ -1,53 +1,93 @@
-import React from 'react';
+import React, {Component, useState} from 'react';
 import * as fe from 'react-feather';
+import { Button, Form, FormGroup, Label, Input, Col } from 'reactstrap';
 import {Link} from 'react-router-dom';
 
-export default function Signout(props){
-    return(
-        <div>
-            <div className="container">
-            <div className="mainform">
-                <div>
-                <Link to="/">
-                    <fe.XCircle className="mb-2" style={{float: "right"}}/>
-                    </Link>
-                </div>
-                  <div>
-                    <h3 className="">
-                      Sign Out
-                    </h3>
-                  </div>
-                  <br />
-                <form>
-                <div className="row">
-                <div className="col-md-12 mb-4">
-                            <label>VisitorId</label><br/>
-                            <input type="text" className="form-control nameinput"/>
-                          </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-12">
-                        <label>Purpose</label><br/>
-                        <textarea
-                        rows="5"
-                        type="text" className="form-control nameinput formtextarea"/>
+class Signout extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            visitorId: '',
+            purpose: ''
+            
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleInputChange(event){
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+
+    }
+
+    handleSubmit(event){
+        console.log('Current State is: ' + JSON.stringify(this.state));
+        alert('Current State is: ' + JSON.stringify(this.state));
+        event.preventDefault();
+    }
+    render(){
+        return(
+            <div>
+                <div className="container">
+                <div className="mainform">
+                    <div>
+                    <Link to="/">
+                        <fe.XCircle className="mb-2" style={{float: "right"}}/>
+                        </Link>
                     </div>
-                </div>
-                <div className="row justify-content-center mt-5">
-                    <div class="btn-group col-12 col-sm-3 align-self-center okbtn" role="group" id="reserveButton"  >
-                        <a role="button" class="btn btn-primary btn-block btn-sm nav-link font-weight-bold ">
-                        OK
-                         </a>
-                    </div>
-                    <div class="btn-group col-12 col-sm-3 align-self-center" role="group" id="reserveButton"  >
-                        <a href="/" role="button" class="btn btn-danger btn-block btn-sm nav-link font-weight-bold ">
-                        Cancel
-                        </a>
-                    </div>
-                  </div>
-            </form>
+                      <div>
+                        <h3 className="">
+                          Sign Out
+                        </h3>
+                      </div>
+                      <br />
+                    <Form onSubmit={this.handleSubmit}>
+                    <FormGroup row>
+                        <Col md={12} mb={4}>
+                                <Label htmlFor="visitorId">VisitorId</Label><br/>
+                                <Input type="text" id="visitorId" name="visitorId" className="form-control nameinput" value={this.state.visitorId}
+                                onChange={this.handleInputChange}/>
+                        </Col>
+                        </FormGroup>
+                     <FormGroup row>
+                     <Col md={12} mb={4}>
+                            <Label htmlFor="purpose">Purpose</Label><br/>
+                            <Input
+                            rows="5"
+                            type="textarea" className="form-control nameinput formtextarea"
+                            name="purpose"
+                            value={this.state.purpose}
+                            onChange={this.handleInputChange}></Input>
+                        </Col>
+                        </FormGroup>
+                    
+                    <FormGroup row className="justify-content-center mt-5">
+                        <div class="btn-group col-12 col-sm-3 align-self-center okbtn" role="group" id="reserveButton"  >
+                            <a role="button" type="submit" class="btn btn-primary btn-block btn-sm nav-link font-weight-bold ">
+                            OK
+                             </a>
+                        </div>
+                        <div class="btn-group col-12 col-sm-3 align-self-center" role="group" id="reserveButton"  >
+                            <a href="/" role="button" class="btn btn-danger btn-block btn-sm nav-link font-weight-bold ">
+                            Cancel
+                            </a>
+                        </div>
+                      </FormGroup>
+                      </Form>
+                
+            </div>
         </div>
     </div>
-</div>
-    )
+        )
+    }
+   
+    
 }
+export default Signout

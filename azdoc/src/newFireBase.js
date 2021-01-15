73a313162f56f125221e4fea9a47e5bdf478d3f8
Vincent Aboagye <68636740+Vincent-Aboagye@users.noreply.github.com>
2021-01-15 09:33:22 +0000

    Firebase config refactored

--- azdoc/src/newFireBase.js
+++ azdoc/src/newFireBase.js
@@ -4,13 +4,13 @@ var newfirebaseApp = firebase.initializeApp({
 // Your web app's Firebase configuration
 apiKey: "AIzaSyA3l3XVAjU2St-gBBd9u4REUFYVqOmVPiI",
 authDomain: "amalitech-visitor-form.firebaseapp.com",
-// databaseURL: "https://amalitech-visitor-form-default-rtdb.europe-west1.firebasedatabase.app",
+databaseURL: "https://amalitech-visitor-form-default-rtdb.europe-west1.firebasedatabase.app",
 projectId: "amalitech-visitor-form",
 storageBucket: "amalitech-visitor-form.appspot.com",
 messagingSenderId: "112111176585",
 appId: "1:112111176585:web:c0a544518bcb2a0f032f58"
 });
 // Initialize Firebase
-var db = newfirebaseApp.firestore();
+var db = newfirebaseApp;
 
 export {db} ;
